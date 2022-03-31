05721380e8a577de44a4c20d8fbe820502c5dc4e
Chougulenik <nikitachougule47@gmail.com>
2022-03-31 15:48:09 -0400

    added

--- src/com/bridgelabz/JUnit.java
+++ src/com/bridgelabz/JUnit.java
@@ -51,7 +51,7 @@ public class JUnit {
 	}
 	
 	public static boolean password(String password) {
-		String pattern = "(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}";
+		String pattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])([#?!@$%^&*-]).{8,}$";
 		if(password.matches(pattern)) {
 			return true;
 		}
