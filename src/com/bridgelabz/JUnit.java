88d6efff9db95a4cbf0c0aa45f9398397e54b09b
Chougulenik <nikitachougule47@gmail.com>
2022-03-31 15:31:36 -0400

    added

--- src/com/bridgelabz/JUnit.java
+++ src/com/bridgelabz/JUnit.java
@@ -11,12 +11,18 @@ public class JUnit {
 		String firstName = scan.nextLine();
 		System.out.println("Enter your last name :");
 		String lastName = scan.nextLine();
+		System.out.println("Enter your Email ID :");
+		String emailId = scan.nextLine();
 		
 		System.out.println("first name is : " +firstName);
 		System.out.println("first name is valid ? " + firstName(firstName));
 		
 		System.out.println("last name is : " +lastName);
 		System.out.println("last name is valid ? " + lastName(lastName));
+		
+		System.out.println("Email ID is : " +emailId);
+		System.out.println("Email ID valid ? " + emailId(emailId));
+		
 		
 
 	}
@@ -27,6 +33,10 @@ public class JUnit {
 	
 	public static boolean lastName(String lastName) {
 		return lastName.matches("[A-Z]{1}[a-z]{3}");
+	}
+	
+	public static boolean emailId(String emailId) {
+		return emailId.matches("[abc](.+){1}[A-Za-z]*+@{1}[bl](.+)[co](.+)[in]$");
 	}
 
 }
